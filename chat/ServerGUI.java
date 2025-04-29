119329a5f44ffaff9e0510fc6af6e5abed4920e2
chetan1417 <114486193+chetan1417@users.noreply.github.com>
2025-04-29 12:51:11 +0530

    Update ServerGUI.java

--- chat/ServerGUI.java
+++ chat/ServerGUI.java
@@ -33,7 +33,7 @@ public class ServerGUI extends JFrame {
         Object[] message = {
                 "Server ID:", idField,
                 "Password:", passField,
-                //"Phone Number:", phoneField
+                "Phone Number:", phoneField
         };
 
         int option = JOptionPane.showConfirmDialog(this, message, "Register Server", JOptionPane.OK_CANCEL_OPTION);
@@ -51,6 +51,7 @@ public class ServerGUI extends JFrame {
         Object[] message = {
                 "Server ID:", idField,
                 "Password:", passField
+            "Phone Number:", phoneField
         };
 
         int option = JOptionPane.showConfirmDialog(this, message, "Server Login", JOptionPane.OK_CANCEL_OPTION);
